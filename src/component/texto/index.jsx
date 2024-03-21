27c2393628a83310a1c61e6cb27b11e11537e5bb
Alisson de Oliveira <72466422+alissonoliveira1@users.noreply.github.com>
2024-03-21 15:23:29 -0300

    portifolio

--- src/component/texto/index.jsx
+++ src/component/texto/index.jsx
@@ -11,37 +11,38 @@ const TypingAnimation = () => {
    ,"I'm a fullstack developer"
    ,""
    ,"Study Software Engineering at UcSal - Universidade Catolica do Salvador"
+   ,"Alisson Oliveira"
  ];
-  const typingSpeed = 50; // Velocidade de digitação em milissegundos
+  const typingSpeed = 50; 
 
   useEffect(() => {
     let currentText = '';
     let index = 0;
 
     const interval = setInterval(() => {
-      // Adiciona a próxima letra à string atual
+     
       currentText += phrases[index].charAt(currentText.length);
 
-      // Atualiza o estado do texto
+     
       setText(currentText);
 
-      // Se a string atual for igual à frase atual, passe para a próxima frase
+      
       if (currentText === phrases[index]) {
         index++;
 
-        // Se chegarmos ao final das frases, reinicie
+       
         if (index === phrases.length) {
           clearInterval(interval);
         } else {
-          // Aguarde 1500ms antes de começar a digitar a próxima frase
+         
           setTimeout(() => {
-            currentText = ''; // Redefine a string atual
+            currentText = ''; 
           }, 2500);
         }
       }
     }, typingSpeed);
 
-    // Limpar o intervalo quando o componente for desmontado
+ 
     return () => clearInterval(interval);
   }, []);
 
